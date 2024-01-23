b4260fed637aa15df10eeaf0a5e24f7e81697ac3
TienTran2003 <tientvt1912003@gmail.com>
2024-01-23 22:21:18 +0700

    create sources

---
+++ src/main/java/com/travel/travelux_be/TraveluxBeApplication.java
@@ -0,0 +1,13 @@
+package com.travel.travelux_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraveluxBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TraveluxBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/travel/travelux_be/TraveluxBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travel.travelux_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraveluxBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
